cb37fd62a653426c20b2849762d720b67756d08c
Elvinmsa <Elvinmsa@github.com>
2021-11-28 18:15:13 -0800

    Add Methods-Functions file

---
+++ 29-11.21/29-11.21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _29_11._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Task 2:
+            
+            //var num = 0;
+            //var n = 5;
+            //var m = 15;
+           
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (1 % 2 == 1)
+            //    {
+            //        num = i;
+            //        Console.WriteLine(num);
+            //    }
+            //}
+
+
+
+
+            //Task 3:
+
+            //var sum = 0;
+            //var n = 10;
+            //var m = 25;
+
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+        }
+    }
+}
+
